1d06170b229e5e5e01c10a1a775a0df189dfae16
Maxwell Da Silva <dayvson@gmail.com>
2013-05-06 12:40:28 -0400

    Added HMAC_SHA1 encode function and BASE64_ENCODE

--- src/vmod_authorization.c
+++ src/vmod_authorization.c
@@ -3,11 +3,7 @@
 #include <string.h>
 #include <stdarg.h>
 #include <stddef.h>
-#define MONGO_HAVE_STDINT
-#include "mongo.h"
-#include "vrt.h"
-#include "bin/varnishd/cache.h"
-#include "vcc_if.h"
+
 /*
  * mhash.h has a habit of pulling in assert(). Let's hope it's a define,
  * and that we can undef it, since Varnish has a better one.
@@ -16,6 +12,13 @@
 #ifdef assert
 #   undef assert
 #endif
+
+#define MONGO_HAVE_STDINT
+#include "mongo.h"
+#include "vrt.h"
+#include "bin/varnishd/cache.h"
+#include "vcc_if.h"
+
 
 typedef struct mongoConfig {
     char *host;
@@ -30,6 +33,21 @@ typedef struct authHeader {
     char *signature;
     char *xdate;
 } auth_header;
+
+enum alphabets {
+    BASE64 = 0,
+    BASE64URL = 1,
+    BASE64URLNOPAD = 2,
+    N_ALPHA
+};
+
+static struct e_alphabet {
+    char *b64;
+    char i64[256];
+    char padding;
+} alphabet[N_ALPHA];
+
+
 
 #define LOG_E(...) fprintf(stderr, __VA_ARGS__);
 #ifdef DEBUG
@@ -98,6 +116,112 @@ get_user_by_token( config_t *cfg, const char* token ) {
     return NULL;
 }
 
+/*
+ * Base64-encode *in (size: inlen) into *out, max outlen bytes. If there is
+ * insufficient space, it will bail out and return -1. Otherwise, it will
+ * null-terminate and return the used space.
+ * The alphabet `a` defines... the alphabet. Padding is optional.
+ * Inspired heavily by gnulib/Simon Josefsson (as referenced in RFC4648)
+ *
+ * XXX: tmp[] and idx are used to ensure the reader (and author) retains
+ * XXX: a limited amount of sanity. They are strictly speaking not
+ * XXX: necessary, if you don't mind going crazy.
+ *
+ * FIXME: outlenorig is silly. Flip the logic.
+ */
+static size_t
+base64_encode (struct e_alphabet *alpha, const char *in,
+        size_t inlen, char *out, size_t outlen)
+{
+    size_t outlenorig = outlen;
+    unsigned char tmp[3], idx;
+
+    if (outlen<4)
+        return -1;
+
+    if (inlen == 0) {
+        *out = '\0';
+        return (1);
+    }
+
+    while (1) {
+        assert(inlen);
+        assert(outlen>3);
+
+        tmp[0] = (unsigned char) in[0];
+        tmp[1] = (unsigned char) in[1];
+        tmp[2] = (unsigned char) in[2];
+
+        *out++ = alpha->b64[(tmp[0] >> 2) & 0x3f];
+
+        idx = (tmp[0] << 4);
+        if (inlen>1)
+            idx += (tmp[1] >> 4);
+        idx &= 0x3f;
+        *out++ = alpha->b64[idx];
+            
+        if (inlen>1) {
+            idx = (tmp[1] << 2);
+            if (inlen>2)
+                idx += tmp[2] >> 6;
+            idx &= 0x3f;
+
+            *out++ = alpha->b64[idx];
+        } else {
+            if (alpha->padding)
+                *out++ = alpha->padding;
+        }
+
+        if (inlen>2) {
+            *out++ = alpha->b64[tmp[2] & 0x3f];
+        } else {
+            if (alpha->padding)
+                *out++ = alpha->padding;
+        }
+
+        /*
+         * XXX: Only consume 4 bytes, but since we need a fifth for
+         * XXX: NULL later on, we might as well test here.
+         */
+        if (outlen<5)
+            return -1;
+
+        outlen -= 4;
+
+        if (inlen<4)
+            break;
+        
+        inlen -= 3;
+        in += 3;
+    }
+
+    assert(outlen);
+    outlen--;
+    *out = '\0';
+    return outlenorig-outlen;
+}
+
+const char *
+vmod_encode_base64(struct sess *sp, const char *msg)
+{
+    char *p;
+    int u;
+    assert(msg);
+    assert(BASE64<N_ALPHA);
+    CHECK_OBJ_NOTNULL(sp, SESS_MAGIC);
+    CHECK_OBJ_NOTNULL(sp->ws, WS_MAGIC);
+    
+    u = WS_Reserve(sp->ws,0);
+    p = sp->ws->f;
+    u = base64_encode(&alphabet[BASE64],msg,strlen(msg),p,u);
+    if (u < 0) {
+        WS_Release(sp->ws,0);
+        return NULL;
+    }
+    WS_Release(sp->ws,u);
+    return p;
+}
+
 const char *
 vmod_encode_sha1(struct sess *sp, const char *msg)
 {
@@ -118,6 +242,82 @@ vmod_encode_sha1(struct sess *sp, const char *msg)
     return p;
 }
 
+const char *
+vmod_encode_hmac(struct sess *sp, const char *key, const char *msg)
+{
+    size_t maclen = mhash_get_hash_pblock(MHASH_SHA1);
+    size_t blocksize = mhash_get_block_size(MHASH_SHA1);
+    unsigned char mac[blocksize];
+    unsigned char *hexenc;
+    unsigned char *hexptr;
+    int j;
+    MHASH td;
+
+    assert(msg);
+    assert(key);
+    CHECK_OBJ_NOTNULL(sp, SESS_MAGIC);
+    CHECK_OBJ_NOTNULL(sp->ws, WS_MAGIC);
+
+    /*
+     * size_t mhash_get_hash_pblock(hashid type);
+     *It returns the block size that the algorithm operates. This
+     * is used in mhash_hmac_init. If the return value is 0 you
+     *shouldn't use that algorithm in  HMAC.
+     */
+    assert(mhash_get_hash_pblock(MHASH_SHA1) > 0);
+
+    td = mhash_hmac_init(MHASH_SHA1, (void *) key, strlen(key),
+        mhash_get_hash_pblock(MHASH_SHA1));
+    mhash(td, msg, strlen(msg));
+    mhash_hmac_deinit(td,mac);
+    
+    /*
+     * HEX-encode
+     */
+    hexenc = WS_Alloc(sp->ws, 2*blocksize+3); // 0x, '\0' + 2 per input
+    if (hexenc == NULL)
+        return NULL;
+    hexptr = hexenc;
+    for (j = 0; j < blocksize; j++) {
+        sprintf(hexptr,"%.2x", mac[j]);
+        hexptr+=2;
+        assert((hexptr-hexenc)<(2*blocksize + 3));
+    }
+    *hexptr = '\0';
+    return hexenc;
+}
+
+/*
+ * Initializes the reverse lookup-table for the relevant base-N alphabet.
+ */
+static void
+digest_alphabet(struct e_alphabet *alpha)
+{
+    int i;
+    const char *p;
+    for (i = 0; i < 256; i++){
+        alpha->i64[i] = -1;
+    }
+    
+    for (p = alpha->b64, i = 0; *p; p++, i++){
+        alpha->i64[(int)*p] = (char)i;
+    }
+
+    if (alpha->padding){
+        alpha->i64[alpha->padding] = 0;
+    }
+}
+
+void 
+init_base64_alphabet()
+{
+    alphabet[BASE64].b64 =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdef"
+        "ghijklmnopqrstuvwxyz0123456789+/";
+    alphabet[BASE64].padding = '=';
+    digest_alphabet(&alphabet[BASE64]);
+}
+
 int
 init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 {
@@ -125,6 +325,7 @@ init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
         priv->priv = make_config("127.0.0.1", 27017, "test.cherry");
         priv->free = free;
     }
+    init_base64_alphabet();
 	return (0);
 }
 
