628f3ea4fd55b2a28f6ecc43e1b370acb1b71889
Maxwell Da Silva <dayvson@gmail.com>
2013-05-19 01:20:15 -0400

    Implemented rate-limit on redis database

--- src/database.h
+++ src/database.h
@@ -15,6 +15,7 @@ typedef int (*database_callback_disconnect) (database_t *database);
 typedef int (*database_callback_connected) (database_t *database);
 typedef int (*database_callback_reconnect) (database_t *database);
 typedef const char *(*database_callback_user_credentials) (database_t *database, const char *token);
+typedef int (*database_callback_ratelimit) (database_t *database, const char *token, const char *secretkey);
 
 /* database Object API */
 database_t *database_new(const char *database_kind);
@@ -23,6 +24,7 @@ void database_free(database_t *database);
 const char *database_get_kind(database_t *database);
 const char *database_get_private_key(database_t *database);
 const char *database_get_public_key(database_t *database);
+const char *database_get_ratelimit_key(database_t *database);
 const char *database_get_table(database_t *database);
 const char *database_get_host(database_t *database);
 const char *database_get_credentials(database_t *database, const char *token);
@@ -32,6 +34,7 @@ void *database_get_data(database_t *database);
 void database_set_kind (database_t *database, const char *kind);
 void database_set_private_key(database_t *database, const char *key);
 void database_set_public_key(database_t *database, const char *key);
+void database_set_ratelimit_key(database_t *database, const char *key);
 void database_set_table(database_t *database, const char *table);
 void database_set_host(database_t *database, const char *host);
 void database_set_port(database_t *database, int port);
@@ -40,13 +43,15 @@ void database_set_data(database_t *database, void *data);
 int database_connect(database_t *database);
 int database_disconnect(database_t *database);
 int database_connected(database_t *database);
-
+int database_isratelimit_allowed(database_t *database, const char *token, const char *secretkey);
 /* Implementation setters */
 void database_callback_set_connect(database_t *database, database_callback_connect fn);
 void database_callback_set_disconnect(database_t *database, database_callback_disconnect fn);
 void database_callback_set_connected(database_t *database, database_callback_connected fn);
 void database_callback_set_reconnect(database_t *database, database_callback_reconnect fn);
 void database_callback_set_user_credentials(database_t *database, database_callback_user_credentials fn);
+void database_callback_set_ratelimit(database_t *database, database_callback_ratelimit fn);
+
 
 /* Databases Initializers */
 void database_init_mongo(database_t *database);
--- src/database_redis.c
+++ src/database_redis.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "database_redis.h"
 #include <hiredis/hiredis.h>
 
@@ -45,7 +47,54 @@ _credentials(database_t *database, const char *token)
 {
     redis_t *redis_config = database_get_data(database);
     redis_config->reply = redisCommand(redis_config->conn, "HGET %s %s", token, database_get_private_key(database));
-    return redis_config->reply->str;
+    if (redis_config->reply->len == 0)
+        return NULL;
+
+    const char *secretkey = strdup(redis_config->reply->str);
+    freeReplyObject(redis_config->reply);
+    return secretkey;
+}
+
+static int
+_check_ratelimit(database_t *database, const char *token, const char *secretkey)
+{
+    redis_t *redis_config = database_get_data(database);
+    redis_config->reply = redisCommand(redis_config->conn, "HGET %s %s", token, database_get_ratelimit_key(database));
+    if (redis_config->reply->len == 0)
+        return 0;
+
+    char *sep = "/";
+    char *tmp_copy = strdup(redis_config->reply->str);
+    freeReplyObject(redis_config->reply);
+    int time_in_seconds = 10;
+    int limit_value = atoi(strtok(tmp_copy, sep));
+
+    char *timef = strtok(NULL, sep);
+    if (strcmp(timef, "s") == 0)
+    {
+        time_in_seconds = 1;
+    }
+    else if (strcmp(timef, "m") == 0)
+    {
+        time_in_seconds = 60;
+    }
+    else if (strcmp(timef, "h") == 0)
+    {
+        time_in_seconds = 3600;
+    }
+
+    redis_config->reply = redisCommand(redis_config->conn, "INCR %s", secretkey);
+    int current_value = redis_config->reply->integer;
+    freeReplyObject(redis_config->reply);
+    if (current_value ==  1)
+    {
+        redis_config->reply = redisCommand(redis_config->conn, "EXPIRE %s 10", secretkey);
+        freeReplyObject(redis_config->reply);
+    }
+    if (current_value > limit_value)
+        return 0;
+
+    return 1;
 }
 
 void
@@ -55,6 +104,5 @@ database_init_redis(database_t *database)
     database_callback_set_disconnect(database, _disconnect);
     database_callback_set_connected(database, _connected);
     database_callback_set_user_credentials(database, _credentials);
-
+    database_callback_set_ratelimit(database, _check_ratelimit);
 }
-
