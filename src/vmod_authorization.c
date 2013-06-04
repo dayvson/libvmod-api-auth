c89ceb783a11b03b167eab5a468847136fdf0792
Maxwell Da Silva <dayvson@gmail.com>
2013-06-04 12:20:45 -0400

    Added phthread_mutex_lock and unlock for database operations

--- src/vmod_authorization.c
+++ src/vmod_authorization.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "vrt.h"
-#include "bin/varnishd/cache.h"
-#include "vcc_if.h"
-#import "authorization_helpers.h"
 #import "database.h"
+#import "authorization_helpers.h"
+
+
+static pthread_mutex_t auth_mtx = PTHREAD_MUTEX_INITIALIZER;
 
 #define LOG_ERR(sess, ...)                                                      \
     if ((sess) != NULL) {                                                       \
@@ -17,84 +17,68 @@
 int
 init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 {
-    init_base64_alphabet();
-    return 0;
+   init_base64_alphabet();
+   
 }
 
 void
 vmod_database(struct sess *sp, struct vmod_priv *priv, const char *database_type)
 {
     if (priv->priv != NULL)
-        database_free (priv->priv);
-    database_t *database = database_new(database_type);
-    priv->priv = database;
+        databasecfg_free (priv->priv);
+    database_cfg_t *db_cfg = databasecfg_new();
+    databasecfg_set_kind(db_cfg, database_type);
+    priv->priv = db_cfg;
 }
-
 
 void
 vmod_database_connect(struct sess *sp, struct vmod_priv *priv, const char *host, int port, const char *table)
 {
-    database_t *database;
+    database_cfg_t *db_cfg;
     if (priv->priv != NULL)
-        database = priv->priv;
+        db_cfg = priv->priv;
 
-    database_set_host(database, host);
-    database_set_port(database, port);
-    database_set_table(database, table);
-
-    priv->priv = database;
+    databasecfg_set_host(db_cfg, host);
+    databasecfg_set_port(db_cfg, port);
+    databasecfg_set_table(db_cfg, table);
+    create_database_pool(db_cfg);
+    priv->priv = db_cfg;
 }
 
 void
 vmod_database_scheme(struct sess *sp, struct vmod_priv *priv, const char *public_key, const char *private_key, const char *ratelimit_key)
 {
-    database_t *database;
+    database_cfg_t *db_cfg;
     if (priv->priv != NULL)
-        database = priv->priv;
-    database_set_private_key(database, private_key);
-    database_set_public_key(database, public_key);
-    database_set_ratelimit_key(database, ratelimit_key);
-    priv->priv = database;
+        db_cfg = priv->priv;
+    databasecfg_set_private_key(db_cfg, private_key);
+    databasecfg_set_public_key(db_cfg, public_key);
+    databasecfg_set_ratelimit_key(db_cfg, ratelimit_key);
+    priv->priv = db_cfg;
 }
 
 unsigned
 vmod_is_valid(struct sess *sp, struct vmod_priv *priv, const char *authorization_header, const char *url, const char *custom_header)
 {
+    
     database_t *database;
+    database_cfg_t *db_cfg;
     auth_header *_header;
     _header = parse_header(authorization_header);
-    char *string_to_sign;
-    unsigned reserved, allocated;
-
-    reserved = WS_Reserve(sp->wrk->ws, 0); /* Reserve some work space */
-    string_to_sign = sp->wrk->ws->f;     /* Front of workspace area */
-    allocated = snprintf(string_to_sign, reserved, "%s-%s", url, custom_header);
-    allocated++;
-    if (allocated > reserved)
-    {
-        /* No space, reset and leave */
-        WS_Release(sp->wrk->ws, 0);
-        return 0;
-    }
-
-    database = priv->priv;
-    database_connect(database);
-
-    /* Update work space with what we've used */
-    WS_Release(sp->wrk->ws, allocated);
+    char string_to_sign[1024];
+    snprintf(string_to_sign, sizeof string_to_sign, "%s-%s", url, custom_header);
+    db_cfg = priv->priv;
+    AZ(pthread_mutex_lock(&auth_mtx));
+    database = get_database_instance(sp);
     char *secretkey = database_get_credentials(database, _header->token);
-
     if (secretkey == NULL)
         return 0;
-    int isok = database_isratelimit_allowed(database, _header->token, secretkey);
-    LOG_ERR(sp, "######################### TOKEN %s ", _header->token);
-    LOG_ERR(sp, "######################### SECRETKEY %s ", secretkey);
-    LOG_ERR(sp, "######################### RATELIMIT %d ", isok);
+   
     char *sign_hmac = encode_hmac(sp, secretkey, string_to_sign);
     char *signed_b64  = encode_base64(sp, sign_hmac);
-    database_disconnect(database);
+    AZ(pthread_mutex_unlock(&auth_mtx));
     if (VRT_strcmp(_header->signature, signed_b64) == 0)
         return 1;
-    
+
     return 0;
 }
